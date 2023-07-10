b4e620798e0eb89cee0b3860ddc018d4a86b49fe
zkc1995 <1486148673@qq.com>
2023-07-10 17:16:33 +0800

    add: tab栏

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,18 @@ const routes: RouterOptions['routes'] = [
   { 
     path: '/recommend',
     component: () => import('@/views/recommend.vue')
+  },
+  { 
+    path: '/singer',
+    component: () => import('@/views/singer.vue')
+  },
+  { 
+    path: '/search',
+    component: () => import('@/views/search.vue')
+  },
+  { 
+    path: '/top-list',
+    component: () => import('@/views/top-list.vue')
   }
 ]
 
---
+++ src/views/singer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    歌手列表
+  </div>
+</template>
+
+<style scoped>
+</style>
