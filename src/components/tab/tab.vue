7c6c1cb05e6b7ceab5cd4084911ed947fa0334b8
zkc1995 <1486148673@qq.com>
2023-07-10 17:09:37 +0800

    add: tab栏

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab">
+    <router-link
+      class="tab-item"
+      v-for="tab in tabList"
+      :key="tab.path"
+      :to="tab.path"
+    >
+      <span class="tab-link">
+        {{tab.name}}
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const tabList = [
+          {
+            name: '推荐',
+            path: '/recommend'
+          },
+          {
+            name: '歌手',
+            path: '/singer'
+          },
+          {
+            name: '排行',
+            path: '/top-list'
+          },
+          {
+            name: '搜索',
+            path: '/search'
+          }
+        ]
+</script>
+
+<style lang="scss" scoped>
+@import "assets/scss/variable.scss";
+.tab {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  font-size: $font-size-medium;
+
+  .tab-item {
+    flex: 1;
+    text-align: center;
+    .tab-link {
+      padding-bottom: 5px;
+      color: $color-text-l;
+    }
+    &.router-link-active {
+      .tab-link {
+        color: $color-theme;
+        border-bottom: 2px solid $color-theme;
+      }
+    }
+  }
+}
+</style>
