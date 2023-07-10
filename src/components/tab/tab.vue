80caf34471ab632d3007ff5836992643b5bc86c6
zkc1995 <1486148673@qq.com>
2023-07-10 17:25:22 +0800

    add: tab栏

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -15,23 +15,23 @@
 
 <script lang="ts" setup>
 const tabList = [
-          {
-            name: '推荐',
-            path: '/recommend'
-          },
-          {
-            name: '歌手',
-            path: '/singer'
-          },
-          {
-            name: '排行',
-            path: '/top-list'
-          },
-          {
-            name: '搜索',
-            path: '/search'
-          }
-        ]
+  {
+    name: "推荐",
+    path: "/recommend",
+  },
+  {
+    name: "歌手",
+    path: "/singer",
+  },
+  {
+    name: "排行",
+    path: "/top-list",
+  },
+  {
+    name: "搜索",
+    path: "/search",
+  },
+];
 </script>
 
 <style lang="scss" scoped>
