eaec33fbf777a76ae4a27ccfb57ca3de64be181a
zkc1995 <1486148673@qq.com>
2023-07-10 17:13:11 +0800

    add: tab栏

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import mHeader from 'components/m-header/m-header.vue'
+import tab from 'components/tab/tab.vue'
 </script>
 
 <template>
   <div @touchmove.prevent>
     <m-header></m-header>
+    <tab></tab>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
